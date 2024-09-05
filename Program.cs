6b117eee0cf446d30f0cd890e7210e16d183e93b
Artur16211 <herrera.alvaradoartu@gmail.com>
2024-09-05 03:58:11 -0600

    shift-jis characters support

--- Program.cs
+++ Program.cs
@@ -22,59 +22,50 @@ namespace PQ2NameTableEditor
 
                 if (arg0.Extension.ToLower() == ".tbl")
                 {
-                    Console.WriteLine($"Attempting to convert { arg0.Name }");
+                    Console.WriteLine($"Attempting to convert {arg0.Name}");
 
-                    using (BinaryObjectReader NAMETBLFile = new BinaryObjectReader(args[0], Endianness.Little, Encoding.UTF8))
+                    using (BinaryObjectReader NAMETBLFile = new BinaryObjectReader(args[0], Endianness.Little, Encoding.GetEncoding("shift-jis")))
                     {
                         List<String> NameTBLStrings = new List<String>();
                         List<UInt16> StringPointers = new List<UInt16>();
 
                         int numOfPointers = NAMETBLFile.ReadUInt16();
-                        // Console.WriteLine($"Expecting { numOfPointers } pointers for file {arg0.Name}\n");
 
                         for (int j = 0; j < numOfPointers; j++)
                         {
                             StringPointers.Add(NAMETBLFile.ReadUInt16());
-                            //if ( i == 0 ) Console.WriteLine($"Current Position after reading pointer is { NAMETBLFile.Position }\n");
                         }
 
-                        long basePos = NAMETBLFile.Position; // save position before strings
-                                                             // Console.WriteLine($"Current Position is 0x{basePos:X4}\n");
+                        long basePos = NAMETBLFile.Position;
 
                         for (int j = 0; j < numOfPointers - 1; j++)
                         {
                             NAMETBLFile.Seek(basePos + StringPointers[j], SeekOrigin.Begin);
 
                             var targetString = NAMETBLFile.ReadString(StringBinaryFormat.NullTerminated);
-                            // Console.WriteLine($"Read string {targetString} at 0x{(basePos + StringPointers[j]):X4}\n");
-
                             NameTBLStrings.Add(targetString);
                         }
 
                         NameTBLStrings.Add("NotExist"); // this doesn't exist in tbl, last pointer goes past EoF
 
-                        //Console.WriteLine($"Current Position after reading all strings is { NAMETBLFile.Position }\n");
-
                         var savePath = arg0.FullName.Replace(arg0.Extension, ".txt");
-                        File.WriteAllLines(savePath, NameTBLStrings, Encoding.UTF8);
+                        File.WriteAllLines(savePath, NameTBLStrings, Encoding.GetEncoding("shift-jis"));
                         Console.WriteLine("Saving " + arg0.FullName.Replace(arg0.Extension, ".txt"));
                     }
                 }
                 else if (arg0.Extension.ToLower() == ".txt")
                 {
-                    // Console.WriteLine("NAME tbl file was input\n");
                     var savePath = arg0.FullName.Replace(arg0.Extension, ".tbl");
-                    // Console.WriteLine(savePath);
 
                     Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-                    using (BinaryObjectWriter NAMETBLFile = new BinaryObjectWriter(savePath, Endianness.Little, Encoding.UTF8))
+                    using (BinaryObjectWriter NAMETBLFile = new BinaryObjectWriter(savePath, Endianness.Little, Encoding.GetEncoding("shift-jis")))
                     {
                         string[] NameTBLStrings = Array.Empty<string>();
 
                         Console.WriteLine("Reading txt file " + arg0.Name);
 
-                        NameTBLStrings = File.ReadAllLines(arg0.FullName, Encoding.UTF8);
+                        NameTBLStrings = File.ReadAllLines(arg0.FullName, Encoding.GetEncoding("shift-jis"));
 
                         NAMETBLFile.WriteUInt16((ushort)NameTBLStrings.Length);
 
@@ -83,21 +74,18 @@ namespace PQ2NameTableEditor
                         for (int j = 0; j < NameTBLStrings.Length; j++)
                         {
                             NAMETBLFile.WriteUInt16(0); // write dummy pointers
-                                                        //if ( i == 0 ) Console.WriteLine($"Current Position after reading pointer is { NAMETBLFile.Position }\n");
                         }
 
                         long basePos = NAMETBLFile.Position; // save position before strings
 
                         NAMETBLFile.WriteString(StringBinaryFormat.NullTerminated, "BLANK"); // original behavior
 
-                        //Write Strings
+                        // Write Strings
                         for (int j = 0; j < NameTBLStrings.Length; j++)
                         {
                             StringPointers.Add(NAMETBLFile.Position - basePos);
                             NAMETBLFile.WriteString(StringBinaryFormat.NullTerminated, NameTBLStrings[j]);
                         }
-
-                        //Console.WriteLine($"Current Position after reading all strings is { NAMETBLFile.Position }\n");
 
                         NAMETBLFile.Seek(2, SeekOrigin.Begin); // seek back to write Pointers
                         for (int j = 0; j < NameTBLStrings.Length; j++)
